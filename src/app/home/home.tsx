34317b9a61f19e3088ee107f459abc8df48e6465
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2024-02-09 14:55:16 +0530

    creating home page

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <div className=" h-screen w-screen bg-stone-200 flex items-center justify-center">
+    <div className="  bg-red-300 h-4/5 w-4/5">
+        <div className="  h-full w-full bg-black/10">
+
+
+        </div>
+        </div>
+   </div>
   )
 }
 
